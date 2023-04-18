43cc704817e8d97f7d475d497012e57c53e9025e
Thiago Domingues <ts.domingues95@gmail.com>
2023-04-18 06:05:19 -0300

    corrected the table in the db referring to the end date of an event. Created file  gerenciar_evento.html and gerenciar_ evento.css

--- eventos/models.py
+++ eventos/models.py
@@ -4,19 +4,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Evento(models.Model):
-    criador = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
-    nome =models.CharField(max_length=200)
+    criador = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    nome = models.CharField(max_length=200)
     descricao = models.TextField()
     data_inicio = models.DateField()
-    data_termino = models.IntegerField()
+    data_termino = models.DateField()
     carga_horaria = models.IntegerField()
-    logo = models.ImageField(upload_to="logos")
+    logo = models.FileField(upload_to="logos")
 
-    #Paletas de cores
+    #paleta de cores
     cor_principal = models.CharField(max_length=7)
     cor_secundaria = models.CharField(max_length=7)
     cor_fundo = models.CharField(max_length=7)
+    
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.nome
     
